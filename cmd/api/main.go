0e2d58328f0193fa5dfcbf49d25bf75eb575e702
Florent Sorel <florent.sorel@gmail.com>
2024-12-28 23:42:45 +0100

    feat: add gha and k8s manifests

---
+++ cmd/api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := []Todo{
+			{1, "Test argocd deployment", false},
+			{2, "Create a new project", false},
+			{3, "Create a new project", false},
+		}
+		js, err := json.MarshalIndent(data, "", "\t")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(js)
+	})
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		return
+	}
+}
